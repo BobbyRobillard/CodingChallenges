2baddbdd6f3155b466b79bd02169c07eb7be632c
Robert Robillard <BobbyRobillard@users.noreply.github.com>
2021-12-27 13:07:58 -0500

    Update LeastCommonMultiple.py

--- CodeWars/Python/LeastCommonMultiple.py
+++ CodeWars/Python/LeastCommonMultiple.py
@@ -1,6 +1,18 @@
 from functools import reduce
 from numpy import lcm as lcm_internal # pip install numpy
 
+# Problem assumes no negative input
+# CORRECT SOLUTION 1 #
+def lcm(*args):
+    if 0 in args: return 0
+    if len(args) == 0: return 1
+    if len(args) == 1: return args[0]
+    lcm_val = 1
+    for arg in args:
+        lcm_val = abs(lcm_val*arg) // math.gcd(lcm_val, arg)
+    return lcm_val
+
+# CORRECT SOLUTION 2 (REFACTORED) #
 def lcm(*args):
     return reduce(lcm_internal, args) if len(args) > 0 else 1
 
