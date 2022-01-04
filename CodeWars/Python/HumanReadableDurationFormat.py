0258e90ce11a460be3004eca70c1d887c78d9a93
Robert Robillard <BobbyRobillard@users.noreply.github.com>
2022-01-04 18:52:56 -0500

    Update HumanReadableDurationFormat.py

--- CodeWars/Python/HumanReadableDurationFormat.py
+++ CodeWars/Python/HumanReadableDurationFormat.py
@@ -12,17 +12,15 @@ def format_duration(seconds):
     seconds %= 60
 
     seconds_text = ("{} second".format(seconds) + ("s" if seconds > 1 else "")) if seconds > 0 else ""
-
     minutes_text = ("{} minute".format(minutes) + ("s" if minutes > 1 else "")) if minutes > 0 else ""
-    minutes_text += (" and " if seconds_text else "") if minutes_text else ""
-
     hours_text = ("{} hour".format(hours) + ("s" if hours > 1 else "")) if hours > 0 else ""
-    hours_text += (", " if minutes_text else "") if hours_text else ""
-
     days_text = ("{} day".format(days) + ("s" if days > 1 else "")) if days > 0 else ""
-    days_text += (", " if hours_text else "") if days_text else ""
-
     years_text = ("{} year".format(years) + ("s" if years > 1 else "")) if years > 0 else ""
-    years_text += (", " if days_text else "") if years else ""
 
-    return years_text + days_text + hours_text + minutes_text + seconds_text or "now"
+    texts = [years_text, days_text, hours_text, minutes_text, seconds_text]
+    texts = list(filter(("").__ne__, texts))
+
+    if len(texts) == 0: return "now"
+    if len(texts) == 1: return texts[0]
+    if len(texts) == 2: return "{0} and {1}".format(texts[0], texts[1])
+    if len(texts) >= 3: return "".join(item + ", " for item in texts[:-2]) + "{0} and {1}".format(texts[-2], texts[-1])
