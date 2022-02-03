24adc862c3beeea5376745fc7cb38d0e2f40add2
Robert Robillard <BobbyRobillard@users.noreply.github.com>
2022-02-03 15:28:18 -0500

    Added More Complete Challenges

---
+++ SimpleProgrammer/Python/CheckForStringRotations.py
@@ -0,0 +1,28 @@
+######################################################################
+# Check if a string (s1) is a rotation of another (s2).
+######################################################################
+
+# Rotate a string by one position
+def rotate(string):
+    return string[-1] + string[0:-1]
+
+
+def check_for_rotations(s1, s2):
+    if len(s2) != len (s1): return False
+    for i in range(len(s1)):
+        s1 = rotate(s1)
+        if s1 == s2: return True
+    return False
+
+
+######################################################################
+# Examples and testing
+######################################################################
+
+s1 = "abcd"
+s2 = "cdab"
+result = check_for_rotations(s1, s2)
+print("Is --> {0} a rotation of --> {1} ? {2}".format(s2, s1, str(result)))
+assert result == True  # Simple unit test
+
+# ---------------------------------------------------------------------
