f5e14d9196ee3f3ee6c9e68dfa2abc075641f34a
Robert Robillard <BobbyRobillard@users.noreply.github.com>
2022-07-13 15:06:40 -0400

    Added More to Running Sum

--- LeetCode/LeetCode75/RunningSum2DArray.py
+++ LeetCode/LeetCode75/RunningSum2DArray.py
@@ -1,6 +1,27 @@
+# Naive approach, sums everything at each step
+# 102 ms | 14.1 MB
 class Solution:
     def runningSum(self, nums: list[int]) -> list[int]:
         return [sum(nums[:i+1]) for i in range(len(nums))]
 
+result = Solution().runningSum([1,2,3,4])
+print(result)
+# ----------------------------------------------------------
+# Better approach, store previous sum values
+# 51 ms	| 14.2 MB
+class Solution:
+    def runningSum(self, nums: list[int]) -> list[int]:
+        sums = [nums[0]]
+        for i in range(1, len(nums)):
+            sums.append(sums[i-1] + nums[i])
+        return sums
+# -----------------------------------------------------------
+# Different approach, does everything in place, not as good on time though.
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        for i in range(1, len(nums)):
+            nums[i] += nums[i - 1]
+        return nums
+# -----------------------------------------------------------
 result = Solution().runningSum([1,2,3,4])
 print(result)
